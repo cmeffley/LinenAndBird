cb768e000558f552b8bb11008262808fc076dbca
Chris Meffley <cmeffley@gmail.com>
2021-08-21 14:36:56 -0400

    Add project files.

---
+++ LinenAndBird/Controllers/BirdController.cs
@@ -0,0 +1,41 @@
+﻿using LinenAndBird.DataAccess;
+using LinenAndBird.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LinenAndBird.Controllers
+{
+    [Route("api/birds")]
+    [ApiController]
+    public class BirdController : ControllerBase
+    {
+        private BirdRepository _repo;
+        public BirdController()
+        {
+            _repo = new BirdRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Bird> GetAllBirds() //use IActionResult instead of IEnumerable
+        {
+            return _repo.GetAll();
+            //return Ok(_repo.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult AddBird(Bird newBird)
+        {
+            if(string.IsNullOrEmpty(newBird.Name) || string.IsNullOrEmpty(newBird.Color))
+            {
+                return BadRequest("Name and Color are required fields");
+            }
+
+            _repo.Add(newBird);
+
+            return Created("/api/birds/1", newBird); 
+            //new bird created at this place. 1 here is just a hard code example since newBird doesn't have an id / we don't have a get single bird
+        }
+
+
+    }
+}
---
+++ LinenAndBird/Controllers/HatsController.cs
@@ -0,0 +1,42 @@
+﻿using LinenAndBird.DataAccess;
+using LinenAndBird.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinenAndBird.Controllers
+{
+    [Route("api/hats")] //exposed at this endpoint
+    [ApiController] //an api controller, so it returns json or xml
+    public class HatsController : ControllerBase
+    {
+        private HatRepository _repo;  //field created for this class
+        public HatsController()
+        {
+            _repo = new HatRepository(); //creating an instance so the field can be used in the class. This constructor is called first then the attributes
+        }
+
+        [HttpGet]
+        public List<Hat> GetAllHats()
+        {
+            return _repo.GetAll();
+        }
+
+        // GET /api/hats/styles/1 -> all open backed hats
+        [HttpGet("styles/{style}")]
+        public IEnumerable<Hat> GetHatsByStyle(HatStyle style)
+        {
+            return _repo.GetByStyle(style);
+        }
+
+        [HttpPost]
+        public void AddAHat(Hat newHat)
+        {
+            _repo.Add(newHat);
+        }
+
+    }
+}
